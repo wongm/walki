add7941a762b92cfe75f3d58b43a403e0dc1591c
Marcus Wong <git@wongm.com>
2014-03-12 20:10:59 +1100

    Add math functions

--- util.php
+++ util.php
@@ -27,4 +27,48 @@ function generateURLWithDevIDAndKey($url)
 	// mash it all together
 	return "http://timetableapi.ptv.vic.gov.au" . $url . "&signature=" . $signature;
 }
+
+function getOffsetLocationBounds($lat, $long, $difference)
+{
+	// http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
+	
+	//Earth's radius, sphere
+	$radius = 6378137;
+	
+	//Coordinate offsets in radians
+	$dLat = $difference / $radius;
+	$dLong = -$difference / ($radius * Cos(Pi() * ($lat / 180)));
+	
+	//OffsetPosition, decimal degrees
+	$bounds->lat1 = $lat + $dLat * 180 / Pi();
+	$bounds->long1 = $long + $dLong * 180 / Pi();
+	
+	//Coordinate offsets in radians
+	$dLat = -$difference / $radius;
+	$dLong = $difference / ($radius * Cos(Pi() * ($lat / 180)));
+	
+	//OffsetPosition, decimal degrees
+	$bounds->lat2 = $lat + $dLat * 180 / Pi();
+	$bounds->long2 = $long + $dLong * 180 / Pi();
+	
+	return $bounds;
+}
+
+function distance($lat1, $long1, $lat2, $long2, $unit) {
+
+  $theta = $long1 - $long2;
+  $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+  $dist = acos($dist);
+  $dist = rad2deg($dist);
+  $miles = $dist * 60 * 1.1515;
+  $unit = strtoupper($unit);
+
+  if ($unit == "K") {
+    return ($miles * 1.609344);
+  } else if ($unit == "N") {
+      return ($miles * 0.8684);
+    } else {
+        return $miles;
+      }
+}
 ?>
