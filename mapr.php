d9ec6ac0ca07721975dfd3788d709fea5b4d831e
Marcus Wong <git@wongm.com>
2014-03-13 20:15:47 +1100

    Now the markers have popups

--- mapr.php
+++ mapr.php
@@ -31,18 +31,21 @@ if ($locationNearestTram != null)
 {
 	$markerNearestTram = "&markers=color:green%7Clabel:T%7C$locationNearestTram->lat,$locationNearestTram->lon";
 	$pathNearestTram = "&path=color:green|weight:5|$originLat,$originLong|$locationNearestTram->lat,$locationNearestTram->lon";
-	
+	$contentNearestTram = "$locationNearestTram->distance metre walk to the nearest tram stop: " . trim($locationNearestTram->location_name);
+
 	// how far do I need to walk to the ticket machine from home?
 	$locationTicketMachine = getNearestPOI($poiTickets, $originLat, $originLong, $boundsOrigin);
 	$markerTicketMachine = "&markers=color:red%7Clabel:M%7C$locationTicketMachine->lat,$locationTicketMachine->lon";
 	$pathTicketMachine = "&path=color:red|weight:5|$originLat,$originLong|$locationTicketMachine->lat,$locationTicketMachine->lon";
-	
+	$contentTicketMachine = "It\'s a $locationTicketMachine->distance metre walk to the nearest myki machine: $locationTicketMachine->business_name at " . trim($locationTicketMachine->location_name) . ", $locationTicketMachine->suburb";
+
 	// where is the nearest tram stop after the ticket machine?
 	$boundsTicketMachine = getOffsetLocationBounds($locationTicketMachine->lat, $locationTicketMachine->lon, $metresMaxWalkingDistance);
 	$locationTramWithTicket = getNearestPOI($poiTrams, $locationTicketMachine->lat, $locationTicketMachine->lon, $boundsTicketMachine);
 	$markerTramWithTicket = "&markers=color:red%7Clabel:T%7C$locationTramWithTicket->lat,$locationTramWithTicket->lon";
 	$pathTramWithTicket = "&path=color:red|weight:5|$locationTicketMachine->lat,$locationTicketMachine->lon|$locationTramWithTicket->lat,$locationTramWithTicket->lon";
-	
+	$contentTramWithTicket = "You then need to walk $locationTramWithTicket->distance metres to the nearest tram stop: " . trim($locationTramWithTicket->location_name);
+
 	$extraMykiDistance = (($locationTicketMachine->distance + $locationTramWithTicket->distance) - $locationNearestTram->distance);
 }
 
@@ -70,17 +73,59 @@ global $config;
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
             
-        var homeLatlng = new google.maps.LatLng(<?php echo $originLat; ?>,<?php echo $originLong; ?>);
-        var homeMarker = new google.maps.Marker({
-	    	position: homeLatlng,
+        var originLatlng = new google.maps.LatLng(<?php echo $originLat; ?>,<?php echo $originLong; ?>);
+        var originMarker = new google.maps.Marker({
+	    	position: originLatlng,
 	    	map: map,
 	    	title:"You are here"
 		});
             
 <?php
 
+if ($locationNearestTram != null)
+{
+?>
+        var nearestTramLatlng = new google.maps.LatLng(<?php echo $locationNearestTram->lat; ?>,<?php echo $locationNearestTram->lon; ?>);
+		var nearestTramInfoWindow = new google.maps.InfoWindow({
+			content: '<?php echo $contentNearestTram ?>'
+  		});
+        var nearestTramMarker = new google.maps.Marker({
+	    	position: nearestTramLatlng,
+	    	map: map,
+	    	title:"Nearest tram stop"
+		});
+		google.maps.event.addListener(nearestTramMarker, 'click', function() {
+    		nearestTramInfoWindow.open(map,nearestTramMarker);
+		});
+		
+        var ticketMachineLatlng = new google.maps.LatLng(<?php echo $locationTicketMachine->lat; ?>,<?php echo $locationTicketMachine->lon; ?>);
+		var ticketMachineInfoWindow = new google.maps.InfoWindow({
+			content: '<?php echo $contentTicketMachine ?>'
+  		});
+        var ticketMachineMarker = new google.maps.Marker({
+	    	position: ticketMachineLatlng,
+	    	map: map,
+	    	title:"Nearest myki machine"
+		});
+		google.maps.event.addListener(ticketMachineMarker, 'click', function() {
+    		ticketMachineInfoWindow.open(map,ticketMachineMarker);
+		});
+		
+        var tramWithTicketLatlng = new google.maps.LatLng(<?php echo $locationTramWithTicket->lat; ?>,<?php echo $locationTramWithTicket->lon; ?>);
+		var tramWithTicketInfoWindow = new google.maps.InfoWindow({
+			content: '<?php echo $contentTramWithTicket ?>'
+  		});
+        var tramWithTicketMarker = new google.maps.Marker({
+	    	position: tramWithTicketLatlng,
+	    	map: map,
+	    	title:"Nearest tram stop"
+		});
+		google.maps.event.addListener(tramWithTicketMarker, 'click', function() {
+    		tramWithTicketInfoWindow.open(map,tramWithTicketMarker);
+		});
 
-
+<?php
+}
 
 ?>
 
