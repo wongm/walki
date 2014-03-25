6b30ddb5ab20c7242cec9378ca452a1e668b5653
Marcus Wong <git@wongm.com>
2014-03-26 07:44:42 +1100

    Split out JSON generation

---
+++ json.php
@@ -0,0 +1,69 @@
+<?php
+
+include_once('util.php');
+
+$metresMaxWalkingDistance = 1500;
+
+$originLat = $_GET['lat'];
+$originLong = $_GET['long'];
+
+if (strlen($originLat) < 1)
+{
+    $originLat = -37.7796654;
+}
+if (strlen($originLong) < 1)
+{
+    $originLong = 144.917969;
+}
+
+// get the bounds
+$boundsOrigin = getOffsetLocationBounds($originLat, $originLong, $metresMaxWalkingDistance);
+
+$poiTickets = 100;
+$poiTrams = 1;
+
+$markerOrigin = "&markers=color:blue%7Clabel:H%7C$originLat,$originLong";
+
+// nearest tram
+$locationNearestTram = getNearestPOI($poiTrams, $originLat, $originLong, $boundsOrigin);
+
+if ($locationNearestTram != null)
+{
+    $contentNearestTram = trim($locationNearestTram->location_name);
+
+    // how far do I need to walk to the ticket machine from home?
+    $locationTicketMachine = getNearestPOI($poiTickets, $originLat, $originLong, $boundsOrigin);
+    $contentTicketMachine = "$locationTicketMachine->business_name at " . trim($locationTicketMachine->location_name) . ", $locationTicketMachine->suburb";
+
+    // where is the nearest tram stop after the ticket machine?
+    $boundsTicketMachine = getOffsetLocationBounds($locationTicketMachine->lat, $locationTicketMachine->lon, $metresMaxWalkingDistance);
+    $locationTramWithTicket = getNearestPOI($poiTrams, $locationTicketMachine->lat, $locationTicketMachine->lon, $boundsTicketMachine);
+    $contentTramWithTicket = trim($locationTramWithTicket->location_name);
+}
+
+global $config;
+
+if ($locationNearestTram != null)
+{
+    $jsondata = array(
+        "nearestTram" => array(
+            "content" => $contentNearestTram,
+            "lat" => $locationNearestTram->lat,
+            "lng" => $locationNearestTram->lon,
+            ),
+        "ticketMachine" => array(
+            "content" => $contentTicketMachine,
+            "lat" => $locationTicketMachine->lat,
+            "lng" => $locationTicketMachine->lon,
+            ),
+        "tramWithTicket" => array(
+            "content" => $contentTramWithTicket,
+            "lat" => $locationTramWithTicket->lat,
+            "lng" => $locationTramWithTicket->lon,
+            )
+    );
+    
+    header('Content-Type: application/json');
+    echo json_encode(array("feed" => $jsondata));
+}
+?>
