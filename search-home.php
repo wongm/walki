dd52857fc7af547694c026e4cbdbfa0ffa119818
Marcus Wong <marcusthomaswong@gmail.com>
2018-06-16 11:40:54 +1000

    Add Google Analytics

--- search-home.php
+++ search-home.php
@@ -83,5 +83,15 @@ $originLong = (double) $_GET['lng'];
         
     <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <script src="https://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+      ga('create', 'UA-94063312-1', 'auto');
+      ga('send', 'pageview');
+    
+    </script>
   </body>
 </html>
